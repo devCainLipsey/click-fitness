f015b4f495924b971d5f570842dc62fc501f3a6f
devCainLipsey <129211827+devCainLipsey@users.noreply.github.com>
2024-02-16 00:46:31 -0600

    Added Sticky NavBar
    Made the NavBar sticky for scrolling
    
    Adjusted color or Error Page Button to match the rest of the buttons

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaDumbbell } from "react-icons/fa";
 import { Link } from "react-scroll";
 import { Link as RouterLink } from "react-router-dom";
 
 const NavBar = () => {
+  const [isSticky, setSticky] = useState(true); // Set initial state to true
+
+  // Function to handle the scroll event
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+
+  // Add scroll event listener directly within the component
+  // This attaches the event listener when the component is mounted
+  window.addEventListener("scroll", handleScroll);
+
   return (
-    <nav className="bg-black py-4 border-b-2 border-red-600">
+    <nav
+      className={`py-4 border-b-2 border-red-600 ${
+        isSticky
+          ? "fixed top-0 inset-x-0 bg-black z-50 transition-all duration-300 ease-in-out"
+          : "bg-black"
+      }`}
+    >
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <div className="flex items-center">
@@ -34,7 +55,7 @@ const NavBar = () => {
             to="about"
             spy={true}
             smooth={true}
-            offset={30}
+            offset={0}
             duration={500}
             className="text-white mr-4 hover:text-red-600 cursor-pointer"
           >
@@ -44,7 +65,7 @@ const NavBar = () => {
             to="plans"
             spy={true}
             smooth={true}
-            offset={-5}
+            offset={-50}
             duration={500}
             className="text-white mr-4 hover:text-red-600 cursor-pointer"
           >
--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import NavBar from "../components/NavBar";
-
 const ErrorPage = () => {
   return (
     <>
-      <NavBar />
       <main className="flex flex-col items-center justify-center h-screen text-center bg-gradient-to-b from-black to-gray-300">
         <div className="bg-black bg-opacity-50 rounded-lg px-8 py-6 text-white text-center border-2 border-red-500">
           <h1 className="text-4xl mb-4">
@@ -17,7 +14,7 @@ const ErrorPage = () => {
             Please click the button to go back to the main page.
           </p>
           <Link to={"/mainpage"}>
-            <button className="inline-flex items-center justify-center px-4 py-2 bg-white text-gray-800 font-medium rounded-lg transition duration-300 hover:bg-gray-200">
+            <button className="inline-flex items-center justify-center px-4 py-2 bg-slate-300 hover:bg-red-600  font-medium rounded-lg">
               Go to main page
             </button>
           </Link>
